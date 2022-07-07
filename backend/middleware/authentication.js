3db382e47d95cabaa4380d664ae1eba18fac8744
MatthieuDeroir <matthieu.deroir@epsi.fr>
2022-07-07 14:04:38 +0200

    dummy

--- backend/middleware/authentication.js
+++ backend/middleware/authentication.js
@@ -5,12 +5,12 @@ const User = db.user;
 
 const verifyToken = (req, res, next) => {
     let token = req.headers["x-access-token"];
-    if (!token){
-        return res.status(403).send({ message: "No token provided!"})
+    if (!token) {
+        return res.status(403).send({ message: "No token provided!" })
     }
     jwt.verify(token, config.secret, (err, decoded) => {
         if (err) {
-            return res.status(401).send({ message :"Unauthorized!" });
+            return res.status(401).send({ message: "Unauthorized!" });
         }
         req.userId = decoded.id;
         next();
@@ -24,24 +24,25 @@ const isAdmin = (req, res, next) => {
         }
         Role.find(
             {
-                _id: {$in: user.roles}
+                _id: { $in: user.roles }
             },
             (err, roles) => {
                 if (err) {
-                    res.status(500).send({ message: err});
+                    res.status(500).send({ message: err });
                     return;
                 }
-                for (let i = 0; i < roles.length; i++){
-                    if (roles[i].name === "admin"){
+                for (let i = 0; i < roles.length; i++) {
+                    if (roles[i].name === "admin") {
                         next();
                         return;
                     }
                 }
-                res.status(403).send({ message: "Require Admin Role !"});
+                res.status(403).send({ message: "Require Admin Role !" });
             }
         );
     });
 };
+//dummy
 
 
 
--- backend/middleware/verifySignUp.js
+++ backend/middleware/verifySignUp.js
@@ -6,16 +6,16 @@ const checkDuplicateUsername = (req, res, next) => {
     User.findOne({
         username: req.body.username
     }).exec((err, user) => {
-            if (err) {
-                res.status(500).send({message: err});
-                return;
-            }
-            if (user) {
-                res.status(400).send({message: "Failed! Username already in use!"})
-                return;
-            }
-            next();
+        if (err) {
+            res.status(500).send({ message: err });
+            return;
         }
+        if (user) {
+            res.status(400).send({ message: "Failed! Username already in use!" })
+            return;
+        }
+        next();
+    }
     )
 }
 
@@ -39,3 +39,4 @@ const verifySignUp = {
 }
 
 module.exports = verifySignUp;
+//dummy
