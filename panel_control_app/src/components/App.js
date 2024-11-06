39765dd5ad92928f5628b94a86026af3b0c2830a
Matthieu Deroir <matthieu.deroir@gmail.com>
2024-11-06 09:10:20 +0100

    feat: :sparkles: front auth complete

--- panel_control_app/src/components/App.js
+++ panel_control_app/src/components/App.js
@@ -1,7 +1,7 @@
 // src/components/App.jsx
-import React, { useState, Suspense, lazy } from 'react';
+import React, { useState, Suspense, lazy, useEffect } from 'react';
 import './App.css';
-import { AuthorizationProvider } from '../Contexts/AuthorizationContext';
+import {AuthorizationProvider, useAuth} from '../Contexts/AuthorizationContext';
 import HealthControl from './Features/HealthControl';
 import PanelManager from './Panels/PanelManager';
 import WebSocketComponent from '../utils/WebsocketComponent';
@@ -9,91 +9,128 @@ import LoadingScreen from './Features/LoadingScreen';
 import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { FaBars, FaTimes } from 'react-icons/fa'; // Import des icônes pour le menu hamburger
+import { FaBars, FaTimes } from 'react-icons/fa';
+import LoginPage from './Login/LoginPage'; // Import LoginPage
+import ProtectedRoute from './ProtectedRoute'; // Import ProtectedRoute component
+import Logout from './Login/LogoutPage'; // Import Logout component
 
-// Chargement paresseux de LogsPage
+function Navigation({ isMobileMenuOpen, setIsMobileMenuOpen }) {
+  const { isAuthenticated } = useAuth();
+
+  return (
+      <ul className={isMobileMenuOpen ? 'nav-menu active' : 'nav-menu'}>
+        {isAuthenticated ? (
+            <>
+              <li className="nav-item">
+                <NavLink
+                    to="/"
+                    className={({ isActive }) => (isActive ? 'nav-links active' : 'nav-links')}
+                    onClick={() => setIsMobileMenuOpen(false)}
+                >
+                  <span className="material-icons">home</span> Accueil
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                    to="/logs"
+                    className={({ isActive }) => (isActive ? 'nav-links active' : 'nav-links')}
+                    onClick={() => setIsMobileMenuOpen(false)}
+                >
+                  <span className="material-icons">assignment</span> Journaux
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                    to="/logout"
+                    className="nav-links"
+                    onClick={() => setIsMobileMenuOpen(false)}
+                >
+                  <span className="material-icons">logout</span> Déconnexion
+                </NavLink>
+              </li>
+            </>
+        ) : (
+            <li className="nav-item">
+              <NavLink
+                  to="/login"
+                  className={({ isActive }) => (isActive ? 'nav-links active' : 'nav-links')}
+                  onClick={() => setIsMobileMenuOpen(false)}
+              >
+                <span className="material-icons">login</span> Connexion
+              </NavLink>
+            </li>
+        )}
+      </ul>
+  );
+}
+
+// Lazy load LogsPage
 const LogsPage = lazy(() => import('./Logs/LogsPage'));
 
 function App() {
-  const [isBackendUp, setIsBackendUp] = useState(null); // Track backend status
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false); // État du menu mobile
+  const [isBackendUp, setIsBackendUp] = useState(null);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen);
   };
 
   return (
-    <AuthorizationProvider>
-      <Router>
-        <div className="App">
-          {/* Barre de Navigation */}
-          <nav className="navbar">
-            <div className="navbar-container">
-              {/* Logo ou Titre */}
-              <NavLink to="/" className="navbar-logo" onClick={() => setIsMobileMenuOpen(false)}>
-                <span className="material-icons">dashboard</span> PanelManager
-              </NavLink>
+      <AuthorizationProvider>
+        <Router>
+          <div className="App">
+            {/* Navigation Bar */}
+            <nav className="navbar">
+              <div className="navbar-container">
+                {/* Logo or Title */}
+                <NavLink to="/" className="navbar-logo" onClick={() => setIsMobileMenuOpen(false)}>
+                  <span className="material-icons">dashboard</span> PanelManager
+                </NavLink>
 
-              {/* Icône du Menu Hamburger */}
-              <div className="menu-icon" onClick={toggleMobileMenu}>
-                {isMobileMenuOpen ? <FaTimes /> : <FaBars />}
-              </div>
-              <div className="health-controls">
+
+                <div className="health-controls">
                   <HealthControl onBackendStatusChange={setIsBackendUp} />
                 </div>
 
-              {/* Liens de Navigation */}
-              <ul className={isMobileMenuOpen ? 'nav-menu active' : 'nav-menu'}>
-                <li className="nav-item">
-                
-                  <NavLink 
-                    to="/" 
-                    className={({ isActive }) => isActive ? 'nav-links active' : 'nav-links'}
-                    onClick={() => setIsMobileMenuOpen(false)}
-                  >
-                    <span className="material-icons">home</span> Accueil
-                  </NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink 
-                    to="/logs" 
-                    className={({ isActive }) => isActive ? 'nav-links active' : 'nav-links'}
-                    onClick={() => setIsMobileMenuOpen(false)}
-                  >
-                    <span className="material-icons">assignment</span> Journaux
-                  </NavLink>
-                </li>
-              </ul>
-            </div>
-          </nav>
+                {/* Navigation Links */}
+                <Navigation isMobileMenuOpen={isMobileMenuOpen} setIsMobileMenuOpen={setIsMobileMenuOpen} />
+              </div>
+            </nav>
 
-          {/* Contenu Principal */}
-          <Routes>
-            <Route path="/" element={
-              <>
-               
-                
-                {isBackendUp === false ? (
-                  <LoadingScreen />
-                ) : (
-                  <>
-                    <PanelManager />
-                  </>
-                )}
-                <WebSocketComponent />
-                <ToastContainer />
-              </>
-            } />
-
-            <Route path="/logs" element={
-              <Suspense fallback={<div className="loading-fallback">Chargement des journaux...</div>}>
-                <LogsPage />
-              </Suspense>
-            } />
-          </Routes>
-        </div>
-      </Router>
-    </AuthorizationProvider>
+            {/* Main Content */}
+            <Routes>
+              <Route
+                  path="/"
+                  element={
+                    <ProtectedRoute>
+                      {isBackendUp === false ? (
+                          <LoadingScreen />
+                      ) : (
+                          <>
+                            <PanelManager />
+                            <WebSocketComponent />
+                            <ToastContainer />
+                          </>
+                      )}
+                    </ProtectedRoute>
+                  }
+              />
+              <Route
+                  path="/logs"
+                  element={
+                    <ProtectedRoute>
+                      <Suspense fallback={<div className="loading-fallback">Chargement des journaux...</div>}>
+                        <LogsPage />
+                      </Suspense>
+                    </ProtectedRoute>
+                  }
+              />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/logout" element={<Logout />} />
+            </Routes>
+          </div>
+        </Router>
+      </AuthorizationProvider>
   );
 }
 
---
+++ panel_control_app/src/components/Login/LogoutPage.js
@@ -0,0 +1,18 @@
+// src/components/Logout.jsx
+import React, { useEffect } from 'react';
+import { useAuth } from '../../Contexts/AuthorizationContext';
+import { useNavigate } from 'react-router-dom';
+
+function Logout() {
+    const { logout } = useAuth();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        logout();
+        navigate('/login');
+    }, [logout, navigate]);
+
+    return null;
+}
+
+export default Logout;
