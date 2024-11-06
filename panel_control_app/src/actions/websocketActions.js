47ad9feccc717e00739dd78f17f7728cb2fca572
Matthieu Deroir <matthieu.deroir@gmail.com>
2024-11-06 11:56:38 +0100

    new libraries

--- panel_control_app/src/actions/websocketActions.js
+++ panel_control_app/src/actions/websocketActions.js
@@ -102,6 +102,11 @@ export const connectWebSocket = () => {
                 type: DISCONNECT_WEBSOCKET,
             });
             console.log('WebSocket connection closed');
+
+            // Attempt to reconnect
+            setTimeout(() => {
+                dispatch(connectWebSocket());
+            }, 5000);
         };
 
         // Optional: Handle WebSocket errors
